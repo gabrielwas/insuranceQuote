fefd80dbc853db6570e47097c5fe3e972606d2eb
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2019-11-13 11:09:45 -0300

    Step 2: Call <UpperBar>

--- src/components/insuranceBase/Insurance.js
+++ src/components/insuranceBase/Insurance.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from "@material-ui/core/styles";
+import UpperBar from "../basicComponents/UpperBar"
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -18,7 +19,7 @@ const Insurance = () => {
 
     return ( 
         <div className={classes.root}>
-            
+            <UpperBar name="Insurance Quotation"/>
         </div>
      );
 }
