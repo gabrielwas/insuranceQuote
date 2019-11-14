76579a64cd793cddd7f74d53f6efb906b4137ee4
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2019-11-13 22:26:30 -0300

    Step 6: Create stateInsurance

---
+++ src/state/stateInsurance.js
@@ -0,0 +1,46 @@
+import { createContext, useContext } from "react";
+
+export const BASIC_INFO = "basicInfo";
+export const CAR_INFORMATION = "carInformation";
+export const CAR_COVERAGE = "carCoverage";
+
+export const StateContext = createContext();
+
+export const useStateValue = () => useContext(StateContext);
+
+export const initialState = {
+  dataDefinitionKey: "InsuranceForm",
+  dataLayoutKey: "InsuranceForm",
+
+  activeStep: 1,
+
+  [BASIC_INFO]: {},
+  [CAR_INFORMATION]: {},
+  [CAR_COVERAGE]: {},
+
+  quotationInfo: {
+    basicPrice: 0,
+    extraPrice: 0
+  },
+
+  emptyFields: []
+};
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case "nextStep":
+      return {
+        ...state,
+        activeStep: state.activeStep + 1
+      };
+
+    case "updateProperty":
+      return {
+        ...state,
+        [action.property]: action.newValue
+      };
+
+    default:
+      break;
+  }
+};
