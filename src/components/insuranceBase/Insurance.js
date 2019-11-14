c95cca90783d7d35dd4ba512d7d6c61386ee1a16
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2019-11-13 23:35:57 -0300

    Step 7: Using useReducer from Sidebar

--- src/components/insuranceBase/Insurance.js
+++ src/components/insuranceBase/Insurance.js
@@ -3,20 +3,29 @@ import UpperBar from "../basicComponents/UpperBar";
 import Sidebar from "./Sidebar";
 import { useStylesInsurance } from "../style";
 import PageBase from "../pages/PageBase";
+import {
+  StateContext,
+  reducer,
+  initialState
+} from "../../state/stateInsurance";
 
 const Insurance = () => {
   const classes = useStylesInsurance();
 
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
   return (
     <div className={classes.root}>
-      <UpperBar name="Insurance Quotation" />
+      <StateContext.Provider value={{ state, dispatch }}>
+        <UpperBar name="Insurance Quotation" />
 
-      <Sidebar />
+        <Sidebar />
 
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <PageBase />
-      </main>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <PageBase />
+        </main>
+      </StateContext.Provider>
     </div>
   );
 };
