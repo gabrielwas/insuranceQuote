bd40d7b9b1350c2728e60cbd83cd1fd909690645
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2019-11-13 23:35:57 -0300

    Step 7: Using useReducer from Sidebar

--- src/components/insuranceBase/Sidebar.js
+++ src/components/insuranceBase/Sidebar.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import SidebarIns from '../basicComponents/SidebarIns';
-import ListItemIns from '../basicComponents/ListItemIns';
-import Divider from '@material-ui/core/Divider';
+import React from "react";
+import SidebarIns from "../basicComponents/SidebarIns";
+import ListItemIns from "../basicComponents/ListItemIns";
+import Divider from "@material-ui/core/Divider";
 
 import StoreMallDirectory from "@material-ui/icons/StoreMallDirectory";
 import DriveEta from "@material-ui/icons/DriveEta";
 import Storage from "@material-ui/icons/Storage";
-
+import { useStateValue } from "../../state/stateInsurance";
 
 const Sidebar = () => {
-    return ( 
-        <SidebarIns>
-            <ListItemIns name="Basic Information" nextStep={1} icon={<StoreMallDirectory/>}/>
-            <ListItemIns name="Car Insurance" nextStep={2} icon={<DriveEta/>}/>
+  const { dispatch } = useStateValue();
 
-            <Divider/>
+  const onClick = nextStep => {
+    dispatch({
+      type: "updateProperty",
+      property: "activeStep",
+      newValue: nextStep
+    });
+  };
 
-            <ListItemIns name="Quotes" nextStep={4} icon={<Storage/>} />
-        </SidebarIns>
-     );
-}
- 
+  return (
+    <SidebarIns>
+      <ListItemIns
+        name="Basic Information"
+        nextStep={1}
+        icon={<StoreMallDirectory />}
+        handleClick={onClick}
+      />
+      <ListItemIns
+        name="Car Insurance"
+        nextStep={2}
+        icon={<DriveEta />}
+        handleClick={onClick}
+      />
+
+      <Divider />
+
+      <ListItemIns
+        name="Quotes"
+        nextStep={4}
+        icon={<Storage />}
+        handleClick={onClick}
+      />
+    </SidebarIns>
+  );
+};
+
 export default Sidebar;
